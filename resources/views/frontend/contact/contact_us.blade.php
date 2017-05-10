33788d5c204b82976e48fccbad77224f670e6a0a
Ukan Saokani <khnz.97@gmail.com>
2017-05-10 07:50:09 +0700

    repair contact page

--- resources/views/frontend/contact/contact_us.blade.php
+++ resources/views/frontend/contact/contact_us.blade.php
@@ -31,10 +31,16 @@
 			</div>
 		</div>
 	</section>
-	<div class="center-align map-place">
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6660.918550976284!2d107.72007585399265!3d-6.937618721017405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c32426bbfd0d%3A0xa15cf57dec9e78a0!2sPondok+Pesantren+Al+-+Ihsan!5e0!3m2!1sen!2sid!4v1494376723617" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+	<!-- <div class="container back-content effect1">
+		<div class="row">
+			<div class="center-align map-place">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6660.918550976284!2d107.72007585399265!3d-6.937618721017405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c32426bbfd0d%3A0xa15cf57dec9e78a0!2sPondok+Pesantren+Al+-+Ihsan!5e0!3m2!1sen!2sid!4v1494376723617" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		</div>
 	</div>
+	
 	<br/>
+	 -->
 	<div class="container back-content effect1">
 		<div class="row">
 			<div class="col-md-6">
@@ -84,6 +90,10 @@
 					<li><i class="fa fa-envelope"></i> <strong>@lang('general.about.email'):</strong> <a href="mailto:alihsanpondokpesantren@gmail.com">alihsanpondokpesantren@gmail.com</a></li>
 				</ul>
 				<hr />
+				<div class="center-align map-place">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6660.918550976284!2d107.72007585399265!3d-6.937618721017405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c32426bbfd0d%3A0xa15cf57dec9e78a0!2sPondok+Pesantren+Al+-+Ihsan!5e0!3m2!1sen!2sid!4v1494376723617" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+				<br>
 			</div>
 		</div>
 	</div>
