724faaad5a0c323fc5b852750e91a1b2cfd5d810
Ukan Saokani <khnz.97@gmail.com>
2017-06-19 06:13:56 +0700

    make page nt found

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,61 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AL Ihsan | Page Not Found</title>
+	<link rel="stylesheet" href="{{ asset($pathp.'assets/general/notfound/style.css') }}">
+	<link href='//fonts.googleapis.com/css?family=Josefin+Sans:400,100,100italic,300,300italic,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+
+	<!-- For-Mobile-Apps-and-Meta-Tags -->
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="Flat Error Page Widget Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" />
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<!-- //For-Mobile-Apps-and-Meta-Tags -->
+<!-- <script src="js/jquery-1.11.1.min.js"></script>  -->
+</head>
+<body>
+	<div class="main">
+		<div class="agile">
+			<div class="agileits_main_grid">
+				<div class="agileits_main_grid_left">
+					<h1>Al Ihsan</h1>
+				</div>
+				<div class="clear"> </div>
+			</div>
+			<div class="w3l">
+				<div class="text">
+					<h1>PAGE NOT FOUND</h1>	
+					
+					<p>You have been tricked into click on a link that can not be found. Please check the url or go to <a href="/">main page</a> and see if you can locate what you are looking for</p>
+				</div>
+				<div class="image">
+					<img class="img-responsive image-resolution" alt="menu icon" src="{{ asset($pathp.'assets/general/images/default/smile.png') }}">
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="footer">
+				<p>&copy; {{ date('Y') }} All Rights Reserved | Design by IT Team of Al Ihsan</p>
+			</div>
+			<div class="wthree">
+				<div class="back">
+					<a href="/">Back to home</a>
+				</div>
+				<div class="social-icons w3agile">
+					<ul>
+						<li>Follow us on :</li>
+						<li><a href="https://www.facebook.com/Pondok-Pesantren-Al-Ihsan-850234935093594/" target="_blank"" class="facebook"><img src="{{ asset($pathp.'assets/general/images/default/fb.png') }}" title="facebook" alt="facebook"></a></li>
+						<li><a href="http://www.instagram.com/alihsan_hits/" target="_blank"" class="facebook"><img src="{{ asset($pathp.'assets/general/images/default/ig.png') }}" width="35" height="35" title="instagram" alt="instagram"></a></li>
+						<div class="clear"></div>
+					</ul>	
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
