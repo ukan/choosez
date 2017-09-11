fa14477c45b837581783e9e8924ec084b5725bcb
Ukan Saokani <khnz.97@gmail.com>
2017-09-11 14:07:14 +0700

    fising bulletin

--- app/Http/Controllers/Backend/Admin/BulletinBoardsController.php
+++ app/Http/Controllers/Backend/Admin/BulletinBoardsController.php
@@ -303,10 +303,10 @@ class BulletinBoardsController extends Controller
                         $file = Input::file('image');            
                         $name = str_random(20). '-' .$file->getClientOriginalName();  
                         $bulletin_board->img_url = date("Y")."/".date("m")."/".date("d")."/".$name;          
-                        // $file->move(public_path().'/storage/news/'.date("Y")."/".date("m")."/".date("d")."/", $name);
+                        $file->move(public_path().'/storage/news/'.date("Y")."/".date("m")."/".date("d")."/", $name);
                         
-                        $path = public_path('/storage/news/'.date("Y")."/".date("m")."/".date("d")."/". $name);
-                        resizeAndSaveImage($file, $path);
+                        // $path = public_path('/storage/news/'.date("Y")."/".date("m")."/".date("d")."/". $name);
+                        // resizeAndSaveImage($file, $path);
                     }
               
                     $bulletin_board->save();
@@ -432,10 +432,10 @@ class BulletinBoardsController extends Controller
                         $file = Input::file('image');            
                         $name = str_random(20). '-' .$file->getClientOriginalName();  
                         $bulletin_board->img_url = date("Y")."/".date("m")."/".date("d")."/".$name;          
-                        // $file->move(public_path().'/storage/news/'.date("Y")."/".date("m")."/".date("d")."/", $name);
+                        $file->move(public_path().'/storage/news/'.date("Y")."/".date("m")."/".date("d")."/", $name);
                         
-                        $path = public_path('/storage/news/'.date("Y")."/".date("m")."/".date("d")."/". $name);
-                        resizeAndSaveImage($file, $path);
+                        // $path = public_path('/storage/news/'.date("Y")."/".date("m")."/".date("d")."/". $name);
+                        // resizeAndSaveImage($file, $path);
                     }
               
                     $bulletin_board->save();
